4f0eacb1284fc744c817a5322109f7e9ac792786
sudeepta_giri <sudeepta_giri@epam.com>
2025-03-19 11:15:47 +0530

    -

--- task09/app/lambdas/api_handler/index.js
+++ task09/app/lambdas/api_handler/index.js
@@ -1,31 +1,28 @@
-import axios from "axios";
-
 exports.handler = async (event) => {
     const path = event.rawPath;
     const method = event.requestContext.http.method;
 
     if (path === "/weather" && method === "GET") {
         try {
-            const response = await axios.get('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m', {
-                params: {
-                    latitude: 50.4375,
-                    longitude: 30.5,
-                    hourly: "temperature_2m,relative_humidity_2m,wind_speed_10m",
-                    current: "temperature_2m,wind_speed_10m",
-                    timezone: "Europe/Kiev"
-                }
-            });
+            const url = "https://api.open-meteo.com/v1/forecast?latitude=50.4375&longitude=30.5&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&current=temperature_2m,wind_speed_10m&timezone=Europe/Kiev";
+
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error("Failed to fetch weather data");
+            }
+
+            const data = await response.json();
 
             return {
                 statusCode: 200,
-                body: JSON.stringify(response.data),
+                body: JSON.stringify(data),
                 headers: { "content-type": "application/json" },
                 isBase64Encoded: false
             };
         } catch (error) {
             return {
                 statusCode: 500,
-                body: JSON.stringify({ error: "Failed to fetch weather data" }),
+                body: JSON.stringify({ error: error.message }),
                 headers: { "content-type": "application/json" }
             };
         }
