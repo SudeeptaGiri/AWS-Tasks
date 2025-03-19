33db89adefa82dbee7dd64627b92c00274c667b0
sudeepta_giri <sudeepta_giri@epam.com>
2025-03-19 10:36:07 +0530

    -

--- task09/app/lambdas/api_handler/index.js
+++ task09/app/lambdas/api_handler/index.js
@@ -1,43 +1,81 @@
 const axios = require('axios');
 
-exports.handler = async (event) => {
+export const handler = async (event) => {
     const path = event.rawPath;
     const method = event.requestContext.http.method;
 
     if (path === "/weather" && method === "GET") {
-        try {
-            const response = await axios.get('https://api.open-meteo.com/v1/forecast', {
-                params: {
-                    latitude: 50.4375,
-                    longitude: 30.5,
-                    hourly: "temperature_2m,relative_humidity_2m,wind_speed_10m",
-                    current: "temperature_2m,wind_speed_10m",
-                    timezone: "Europe/Kiev"
-                }
-            });
-
-            return {
-                statusCode: 200,
-                body: JSON.stringify(response.data),
-                headers: { "content-type": "application/json" },
-                isBase64Encoded: false
-            };
-        } catch (error) {
-            return {
-                statusCode: 500,
-                body: JSON.stringify({ error: "Failed to fetch weather data" }),
-                headers: { "content-type": "application/json" }
-            };
-        }
+        return {
+            "statusCode": 200,
+            "body": {
+              "latitude": 50.4375,
+              "longitude": 30.5,
+              "generationtime_ms": 0.025033950805664062,
+              "utc_offset_seconds": 7200,
+              "timezone": "Europe/Kiev",
+              "timezone_abbreviation": "EET",
+              "elevation": 188.0,
+              "hourly_units": {
+                  "time": "iso8601",
+                  "temperature_2m": "°C",
+                  "relative_humidity_2m": "%",
+                  "wind_speed_10m": "km/h"
+              },
+              "hourly": {
+                  "time": [
+                      "2023-12-04T00:00",
+                      "2023-12-04T01:00",
+                      "2023-12-04T02:00",
+                      "..."
+                  ],
+                  "temperature_2m": [
+                      -2.4,
+                      -2.8,
+                      -3.2,
+                      "..."
+                  ],
+                  "relative_humidity_2m": [
+                      84,
+                      85,
+                      87,
+                      "..."
+                  ],
+                  "wind_speed_10m": [
+                      7.6,
+                      6.8,
+                      5.6,
+                      "..."
+                  ]
+              },
+              "current_units": {
+                  "time": "iso8601",
+                  "interval": "seconds",
+                  "temperature_2m": "°C",
+                  "wind_speed_10m": "km/h"
+              },
+              "current": {
+                  "time": "2023-12-04T07:00",
+                  "interval": 900,
+                  "temperature_2m": 0.2,
+                  "wind_speed_10m": 10.0
+              }
+            },
+            "headers": {
+              "content-type": "application/json"
+            },
+            "isBase64Encoded": false
+          }
     }
 
-    return {
-        statusCode: 400,
-        body: JSON.stringify({
-            statusCode: 400,
-            message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
-        }),
-        headers: { "content-type": "application/json" },
-        isBase64Encoded: false
-    };
+    return  {
+        "statusCode": 400,
+        "body": {
+          "statusCode": 400,
+          "message": `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
+        },
+        "headers": {
+          "content-type": "application/json"
+        },
+        "isBase64Encoded": false
+      }
 };
